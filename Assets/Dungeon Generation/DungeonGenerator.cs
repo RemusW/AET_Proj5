43f0a44d4be44ee7d2b2f3d807171ccaab08e8e8
RemusW <rwong2100@hotmail.com>
2021-04-07 17:37:11 -0500

    added boss and spikes spawning

--- Assets/Dungeon Generation/DungeonGenerator.cs
+++ Assets/Dungeon Generation/DungeonGenerator.cs
@@ -18,11 +18,17 @@ public class DungeonGenerator : MonoBehaviour
     [SerializeField]
     private Tile treasureTile;
     [SerializeField]
+    private Tile bossTile;
+    [SerializeField]
+    private Tile bossBackgroundTile;
+    [SerializeField]
     private Tilemap groundMap;
     [SerializeField]
     private Tilemap pitMap;
     [SerializeField]
     private Tilemap wallMap;
+    [SerializeField]
+    private Tilemap addMap;
     [SerializeField]
     private GameObject player;
     [SerializeField]
@@ -37,8 +43,10 @@ public class DungeonGenerator : MonoBehaviour
 
     private int routeCount = 0;
 
-    private void Start()
-    {
+    private void Start() {
+        bossBackgroundTile.name = "bossBackground";
+        groundTile.name = "ground";
+
         int x = 0;
         int y = 0;
         int routeLength = 0;
@@ -49,6 +57,20 @@ public class DungeonGenerator : MonoBehaviour
         NewRoute(x, y, routeLength, previousPos);
 
         FillWalls();
+        addFeatures();
+    }
+
+    private void addFeatures() {
+        BoundsInt bounds = groundMap.cellBounds;
+        for (int xMap = bounds.xMin; xMap <= bounds.xMax; xMap++) {
+            for (int yMap = bounds.yMin; yMap <= bounds.yMax; yMap++) {
+                Vector3Int pos = new Vector3Int(xMap, yMap, 0);
+                TileBase tileAt = groundMap.GetTile(pos);
+                if (tileAt != null && Random.Range(1,100) < 2 && tileAt.name != "bossBackground") {
+                    addMap.SetTile(pos, treasureTile);
+                }
+            }
+        }
     }
 
     private void FillWalls()
@@ -64,15 +86,12 @@ public class DungeonGenerator : MonoBehaviour
                 TileBase tile = groundMap.GetTile(pos);
                 TileBase tileBelow = groundMap.GetTile(posBelow);
                 TileBase tileAbove = groundMap.GetTile(posAbove);
-                if (tile == null)
-                {
+                if (tile == null) {
                     pitMap.SetTile(pos, pitTile);
-                    if (tileBelow != null)
-                    {
+                    if (tileBelow != null) {
                         wallMap.SetTile(pos, topWallTile);
                     }
-                    else if (tileAbove != null)
-                    {
+                    else if (tileAbove != null) {
                         wallMap.SetTile(pos, botWallTile);
                     }
                 }
@@ -80,13 +99,13 @@ public class DungeonGenerator : MonoBehaviour
         }
     }
 
+    bool bossExist = false;
+
     private void NewRoute(int x, int y, int routeLength, Vector2Int previousPos)
     {
-        if (routeCount < maxRoutes)
-        {
+        if (routeCount < maxRoutes) {
             routeCount++;
-            while (++routeLength < maxRouteLength)
-            {
+            while (++routeLength < maxRouteLength) {
                 //Initialize
                 bool routeUsed = false;
                 int xOffset = x - previousPos.x; //0
@@ -97,15 +116,12 @@ public class DungeonGenerator : MonoBehaviour
                 previousPos = new Vector2Int(x, y);
 
                 //Go Straight
-                if (Random.Range(1, 100) <= deviationRate)
-                {
-                    if (routeUsed)
-                    {
+                if (Random.Range(1, 100) <= deviationRate) {
+                    if (routeUsed) {
                         GenerateSquare(previousPos.x + xOffset, previousPos.y + yOffset, roomSize);
                         NewRoute(previousPos.x + xOffset, previousPos.y + yOffset, Random.Range(routeLength, maxRouteLength), previousPos);
                     }
-                    else
-                    {
+                    else {
                         x = previousPos.x + xOffset;
                         y = previousPos.y + yOffset;
                         GenerateSquare(x, y, roomSize);
@@ -114,15 +130,12 @@ public class DungeonGenerator : MonoBehaviour
                 }
 
                 //Go left
-                if (Random.Range(1, 100) <= deviationRate)
-                {
-                    if (routeUsed)
-                    {
+                if (Random.Range(1, 100) <= deviationRate) {
+                    if (routeUsed) {
                         GenerateSquare(previousPos.x - yOffset, previousPos.y + xOffset, roomSize);
                         NewRoute(previousPos.x - yOffset, previousPos.y + xOffset, Random.Range(routeLength, maxRouteLength), previousPos);
                     }
-                    else
-                    {
+                    else {
                         y = previousPos.y + xOffset;
                         x = previousPos.x - yOffset;
                         GenerateSquare(x, y, roomSize);
@@ -130,15 +143,12 @@ public class DungeonGenerator : MonoBehaviour
                     }
                 }
                 //Go right
-                if (Random.Range(1, 100) <= deviationRate)
-                {
-                    if (routeUsed)
-                    {
+                if (Random.Range(1, 100) <= deviationRate) {
+                    if (routeUsed) {
                         GenerateSquare(previousPos.x + yOffset, previousPos.y - xOffset, roomSize);
                         NewRoute(previousPos.x + yOffset, previousPos.y - xOffset, Random.Range(routeLength, maxRouteLength), previousPos);
                     }
-                    else
-                    {
+                    else {
                         y = previousPos.y - xOffset;
                         x = previousPos.x + yOffset;
                         GenerateSquare(x, y, roomSize);
@@ -146,26 +156,38 @@ public class DungeonGenerator : MonoBehaviour
                     }
                 }
 
-                if (!routeUsed)
-                {
+                if (!routeUsed) {
                     x = previousPos.x + xOffset;
                     y = previousPos.y + yOffset;
                     GenerateSquare(x, y, roomSize);
                 }
             }
+            if (!bossExist) {
+                Debug.Log("Generating room");
+                bossExist = true;
+                GenerateBossRoom(x, y);
+            }
         }
     }
 
-    private void GenerateSquare(int x, int y, int radius)
-    {
-        for (int tileX = x - radius; tileX <= x + radius; tileX++)
-        {
-            for (int tileY = y - radius; tileY <= y + radius; tileY++)
-            {
+    private void GenerateSquare(int x, int y, int radius) {
+        for (int tileX = x - radius; tileX <= x + radius; tileX++) {
+            for (int tileY = y - radius; tileY <= y + radius; tileY++) {
                 Vector3Int tilePos = new Vector3Int(tileX, tileY, 0);
                 groundMap.SetTile(tilePos, groundTile);
             }
         }
     }
 
+    private void GenerateBossRoom(int x, int y) {
+        int radius = 10;
+        for (int tileX = x - radius; tileX <= x + radius; tileX++) {
+            for (int tileY = y - radius; tileY <= y + radius; tileY++) {
+                Vector3Int tilePos = new Vector3Int(tileX, tileY, 0);
+                groundMap.SetTile(tilePos, bossBackgroundTile);
+            }
+        }
+        Vector3Int centerPos = new Vector3Int(x, y, 0);
+        addMap.SetTile(centerPos, bossTile);
+    }
 }
