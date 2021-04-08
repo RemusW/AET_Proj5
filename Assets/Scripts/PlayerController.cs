cd8618f019fe45b6ee4c83c835aafab7c2a2b1db
orange-flower <jn.147261@gmail.com>
2021-04-08 17:28:23 -0500

    replaced boss tile

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,48 +20,46 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //get horizontal and vertical axis positions
         horizontal = Input.GetAxisRaw("Horizontal") * movementSpd;
         vertical = Input.GetAxisRaw("Vertical") * movementSpd;
 
+        //play animations based on player button input 
         if (Input.GetButtonDown("Horizontal"))
         {
+            //play right animation when x is positive
             if (horizontal > 0)
             {
                 playerAnimator.SetFloat("RightSpd", horizontal);
-                //transform.position = transform.position + new Vector3(movementSpd, 0, 0);
-                //transform.Translate(movementSpd, 0, 0);
             }
-            else
+            else //play left animation when x is negative
             {
                 playerAnimator.SetFloat("LeftSpd", horizontal * (-movementSpd));
-                //transform.position = transform.position + new Vector3(-movementSpd, 0, 0);
-                //transform.Translate(-movementSpd, 0, 0);
             }
         }
 
+        //stop animations when button is released
         if (Input.GetButtonUp("Horizontal"))
         {
             playerAnimator.SetFloat("RightSpd", 0);
             playerAnimator.SetFloat("LeftSpd", 0);
         }
 
+        //play animations based on player button input
         if (Input.GetButtonDown("Vertical"))
         {
+            //play up animation when y is positive
             if (vertical > 0)
             {
                 playerAnimator.SetFloat("UpSpd", vertical);
-                //transform.position = transform.position + new Vector3(0, movementSpd, 0);
-                //transform.Translate(0, movementSpd, 0);
             }
-            else
+            else //play down animation when y is negative
             {
                 playerAnimator.SetFloat("DownSpd", vertical * (-movementSpd));
-                //transform.position = transform.position + new Vector3(0, -movementSpd, 0);
-                //transform.Translate(0, -movementSpd, 0);
-
             }
         }
 
+        //stop animations when button is released
         if (Input.GetButtonUp("Vertical"))
         {
             playerAnimator.SetFloat("UpSpd", 0);
@@ -71,9 +69,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal, vertical);
-
-        
+        rb.velocity = new Vector2(horizontal, vertical);    
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -88,63 +84,3 @@ public class PlayerController : MonoBehaviour
         cam.GetComponent<CameraZoom>().updateNumTriggers(-1, col.transform);
     }
 }
-
-//     public GameObject player;
-//     public Animator playerAnimator;
-
-//     private float horizontal;
-//     private float vertical;
-//     public float movementSpd = 0.5f;
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         //get axis values
-//         horizontal = Input.GetAxisRaw("Horizontal");
-//         vertical = Input.GetAxisRaw("Vertical");
-
-//         if (Input.GetButtonDown("Horizontal"))
-//         {
-//             if (horizontal > 0)
-//             {
-//                 playerAnimator.SetFloat("RightSpd", horizontal);
-//                 //transform.position = transform.position + new Vector3(movementSpd, 0, 0);
-//                 transform.Translate(movementSpd, 0, 0);
-//             }
-//             else
-//             {
-//                 playerAnimator.SetFloat("LeftSpd", horizontal * (-movementSpd));
-//                 //transform.position = transform.position + new Vector3(-movementSpd, 0, 0);
-//                 transform.Translate(-movementSpd, 0, 0);
-//             }
-//         }
-
-//         if (Input.GetButtonUp("Horizontal"))
-//         {
-//             playerAnimator.SetFloat("RightSpd", 0);
-//             playerAnimator.SetFloat("LeftSpd", 0);
-//         }
-
-//         if (Input.GetButtonDown("Vertical"))
-//         {
-//             if (vertical > 0)
-//             {
-//                 playerAnimator.SetFloat("UpSpd", vertical);
-//                 //transform.position = transform.position + new Vector3(0, movementSpd, 0);
-//                 transform.Translate(0, movementSpd, 0);
-//             }
-//             else
-//             {
-//                 playerAnimator.SetFloat("DownSpd", vertical * (-movementSpd));
-//                 //transform.position = transform.position + new Vector3(0, -movementSpd, 0);
-//                 transform.Translate(0, -movementSpd, 0);
-//             }
-//         }
-        
-//         if (Input.GetButtonUp("Vertical"))
-//         {
-//             playerAnimator.SetFloat("UpSpd", 0);
-//             playerAnimator.SetFloat("DownSpd", 0);
-//         }
-
-//     }
